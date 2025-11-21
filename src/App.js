bf5d2d115ee142340109d600e48cdf4cf48c9558
Mhmd Shami <mhmddshamii@gmail.com>
2025-11-21 21:32:05 +0200

    setup routs and rendering proper component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Users from "./components/Users";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex justify-center w-full h-full ">
+      <div className="flex justify-center w-full h-full flex-col items-center p-4 box-border">
         <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Products.js
@@ -0,0 +1,8 @@
+export default function Products() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Products Page</h2>
+      <p>Welcome to the Products page!</p>
+    </div>
+  );
+}
